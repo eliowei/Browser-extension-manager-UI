e73d4511b0d8adbaa791c1afffb080a8b7f3d494
ElioWei <zxc160604@gmail.com>
2025-03-31 17:49:21 +0800

    add commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
+// card 陣列是用來存放從 JSON 檔案讀取的擴充功能資料
 const card = [];
+// listFilterType 是用來控制列表過濾的變數，0 代表全部，1 代表啟用，2 代表停用
 let listFilterType = 0;
+// 列表過濾容器
 const cardSection = document.querySelector(".extension__list");
-
+// 風格按鈕
 const themeButton = document.querySelector(".theme__button");
+// 風格按鈕的圖片
 const themeButtonImg = document.querySelector(".theme__button img");
+// 風格模式切換
 let darkMode = false;
+
+// 列表過濾按鈕
+const listFilterButton = document.querySelectorAll(".button__content button");
 // 透過fetch 讀取到json檔，並放進指定的陣列
 async function fetchExtensions() {
   try {
@@ -16,13 +24,15 @@ async function fetchExtensions() {
     console.log(error);
   }
 }
-
+// 渲染擴充功能列表
 function renderExtensions() {
-  cardSection.innerHTML = ""; // 清空 DOM
+  // 清空列表
+  cardSection.innerHTML = "";
+  // 根據 listFilterType 過濾卡片
   const filterCard = card.filter((e) =>
     listFilterType == 1 ? e.isActive : listFilterType == 2 ? !e.isActive : e
   );
-
+  // 根據過濾後的卡片數量來決定顯示的內容
   filterCard.forEach((e, index) => {
     const originalIndex = card.findIndex((item) => item === e);
 
@@ -54,11 +64,15 @@ function renderExtensions() {
         </div>
       </div>
       `;
+    // 將卡片 HTML 插入到列表中
     cardSection.insertAdjacentHTML("beforeend", cardHTML);
   });
 }
+// 讀取 JSON 檔案
+// 並將資料渲染到列表中
 fetchExtensions();
 
+// 處理卡片的點擊事件
 cardSection.addEventListener("click", (event) => {
   // 找到被點擊的元素在卡片陣列中的索引
   const cards = event.target.closest(".card");
@@ -89,8 +103,7 @@ cardSection.addEventListener("click", (event) => {
   }
 });
 
-const listFilterButton = document.querySelectorAll(".button__content button");
-
+// 處理列表過濾按鈕的點擊事件
 listFilterButton.forEach((e, index) => {
   e.addEventListener("click", () => {
     listFilterButton[listFilterType].classList.remove("active");
@@ -99,15 +112,16 @@ listFilterButton.forEach((e, index) => {
     renderExtensions();
   });
 });
-
+// 列表過濾按鈕切換後的狀態
 function changelistFilter() {
   listFilterButton[listFilterType].classList.add("active");
 }
 changelistFilter();
 
+// 處理風格切換按鈕的點擊事件
 themeButton.addEventListener("click", () => {
   darkMode = !darkMode;
-  console.log(darkMode);
+
   if (darkMode) {
     themeButtonImg.src = "./assets/images/icon-sun.svg";
     document.body.style.background =
